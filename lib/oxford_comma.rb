52d59d4dc7b71b81c3e580c720acca4ef0781b11
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-03-24 01:10:12 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+	if array.size == 1
+		array.join
+	elsif array.size == 2
+		array.join(" and ")
+	elsif array.size >= 3
+		array[-1].insert(0, "and ") #would this also work if "dragon fruit" was not an array itself?
+		array.join(", ")
+end
 end
